d83a123556265fa4d0c0d2bddba19b824d5c2bc8
Mario Finelli <mario@finel.li>
2017-10-31 17:09:05 -0400

    Update bootstrap with encryption and mkfs

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,3 +25,37 @@ EOF
 
 # let's check one more time that the disks are partitioned as expected
 gdisk -l $INSTALL_DISK
+
+# encrypt the main partition: AES256 (e.g. /dev/sda2)
+cryptsetup --cipher aes-xts-plain64 --key-size 512 --hash sha512 \
+  --iter-time 5000 --use-random --verify-passphrase luksFormat ${INSTALL_DISK}2
+
+# get some much needed info
+read -p "How many swap G? "
+SWAPG="$REPLY"
+read -p "How many root G? "
+ROOTG="$REPLY"
+
+# do the LVM boogie
+cryptsetup luksOpen /dev/sda2 lvm
+pvcreate /dev/mapper/lvm
+vgcreate crypt /dev/mapper/lvm
+lvcreate -L ${SWAPG}G crypt -n swap
+lvcreate -L ${ROOTG}G crypt -n root
+lvcreate -l 100%FREE crypt -n home
+
+# make some filesystems
+mkfs.vfat -F32 ${INSTALL_DISK}1
+mkfs.btrfs /dev/mapper/crypt-root
+mkfs.btrfs /dev/mapper/crypt-home
+mkswap /dev/mapper/crypt-swap
+
+# get everything ready
+swapon /dev/crypt/swap
+mount -o compress=lzo /dev/crypt/root /mnt
+mkdir /mnt/{boot,home}
+mount ${INSTALL_DISK}1 /mnt/boot
+mount  -o compress=lzo /dev/crypt/home /mnt/home
+
+# do the needful
+pacstrap -i /mnt base base-devel
