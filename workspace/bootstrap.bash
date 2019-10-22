ca4692385daf8a468ee4fd1f03100be7419860e3
Mario Finelli <mario@finel.li>
2019-10-22 18:02:10 -0400

    Actually run the playbook

--- workspace/bootstrap.bash
+++ workspace/bootstrap.bash
@@ -5,7 +5,14 @@ if [[ $(id -u) -ne 0 ]]; then
   exit 1
 fi
 
+if [[ ! -f workspace.yml ]]; then
+  echo >&2 "unable to find workspace playbook"
+  exit 1
+fi
+
 yum install python{,-pip}
 pip install ansible==2.8.6
+
+ansible-playbook ./workspace.yml
 
 exit 0
