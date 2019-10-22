e15cb88125ae9889e48ce5f3edc0267fed6390de
Mario Finelli <mario@finel.li>
2019-10-22 18:21:52 -0400

    Don't run bootstrap as root

--- workspace/bootstrap.bash
+++ workspace/bootstrap.bash
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-if [[ $(id -u) -ne 0 ]]; then
-  echo >&2 "must run as root!"
+if [[ $(id -u) -eq 0 ]]; then
+  echo >&2 "must not run as root!"
   exit 1
 fi
 
@@ -10,9 +10,13 @@ if [[ ! -f workspace.yml ]]; then
   exit 1
 fi
 
-yum install python{,-pip}
-pip install ansible==2.8.6
 
-ansible-playbook ./workspace.yml
+# prompt for password right away
+sudo echo -n
+
+sudo yum install python{,-pip}
+sudo pip install ansible=="$ansver"
+
+sudo ansible-playbook --extra-vars whoami="$(whoami)" ./workspace.yml
 
 exit 0
