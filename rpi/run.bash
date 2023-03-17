176caf4d0d66fd261ce0e723da1a450081832f69
Mario Finelli <mario@finel.li>
2023-03-17 15:09:50 +0100

    Use collections provided by ansible package

--- rpi/run.bash
+++ rpi/run.bash
@@ -12,7 +12,6 @@ else
   fde=false
 fi
 
-ansible-galaxy collection install -U -r ../requirements.yml
 ansible-playbook --inventory ../localhost \
   --extra-vars fde=$fde --extra-vars whoami="$(whoami)" \
   --extra-vars whoami_home="$HOME" rpi.yml
