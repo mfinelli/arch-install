fffb755b2b4f4e1930ac96b7f5ee290282ef4cf9
Mario Finelli <mario@finel.li>
2023-04-23 15:33:11 +0200

    Simplify fde host detection

--- rpi/run.bash
+++ rpi/run.bash
@@ -5,8 +5,13 @@ if [[ $# -ne 0 ]]; then
   exit 1
 fi
 
-hn="$(cat /etc/hostname)"
-if [[ $hn == testpi || $hn == parkpi || $hn == raipi ]]; then
+declare -a fde_enabled_hosts=(homepi parkpi raipi testpi)
+
+# https://unix.stackexchange.com/a/177589
+declare -A fde_enabled_host
+for host in "${fde_enabled_hosts[@]}"; do fde_enabled_host[$host]=1; done
+
+if [[ -n ${fde_enabled_host[$(cat /etc/hostname)]} ]]; then
   fde=true
 else
   fde=false
