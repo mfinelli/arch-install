6b30ac53a87d3431b415975b46fe438a36f02835
Mario Finelli <mario@finel.li>
2023-04-23 19:08:31 +0200

    Track tailscale exit node status

--- rpi/run.bash
+++ rpi/run.bash
@@ -5,19 +5,32 @@ if [[ $# -ne 0 ]]; then
   exit 1
 fi
 
+hn="$(cat /etc/hostname)"
 declare -a fde_enabled_hosts=(homepi parkpi raipi testpi)
+declare -a tailscale_exit_nodes=(parkpi raipi)
 
 # https://unix.stackexchange.com/a/177589
 declare -A fde_enabled_host
 for host in "${fde_enabled_hosts[@]}"; do fde_enabled_host[$host]=1; done
+declare -A tailscale_exit_node
+for host in "${tailscale_exit_nodes[@]}"; do tailscale_exit_node[$host]=1; done
 
-if [[ -n ${fde_enabled_host[$(cat /etc/hostname)]} ]]; then
+if [[ -n ${fde_enabled_host[$hn]} ]]; then
   fde=true
 else
   fde=false
 fi
 
-ansible-playbook --inventory ../localhost --extra-vars fde=$fde \
-  --extra-vars whoami="$(whoami)" rpi.yml
+if [[ -n ${tailscale_exit_node[$hn]} ]]; then
+  tailscale_exit_node=true
+else
+  tailscale_exit_node=false
+fi
+
+ansible-playbook --inventory ../localhost \
+  --extra-vars fde=$fde \
+  --extra-vars tailscale_exit_node=$tailscale_exit_node \
+  --extra-vars whoami="$(whoami)" \
+  rpi.yml
 
 exit 0
